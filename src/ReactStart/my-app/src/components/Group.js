60bf4123aba1504fd1f285842ab0f09691529336
MelvinSu <msu009@ucr.edu>
2019-05-20 00:40:35 -0700

    added join group function

--- src/ReactStart/my-app/src/components/Group.js
+++ src/ReactStart/my-app/src/components/Group.js
@@ -5,103 +5,176 @@ import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
 import img_placeholder from './pics/img_placeholder.png';
 import group_placeholder from './pics/group_placeholder.png';
+import fire, {auth} from '../fire';
 
-function Group() {
-  return (
-    <header>
-      <Navbar bg="light" expand="ex-lg">
-        <Navbar.Brand href="./main">Meetup</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav className="ml-auto">
-            <NavItem className="ml-auto">
-              <Nav.Link href="./profile">Profile</Nav.Link>
-            </NavItem>
-            <NavItem className="ml-auto">
-              <Nav.Link className="ml-auto" href="./create_group">Create Group</Nav.Link>
-            </NavItem>
-            <NavItem className="ml-auto">
-              <Nav.Link className="ml-auto" href="#">Logout</Nav.Link>
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <main>
-        <div class="container">
-          <div class="row group_row">
-            <div class="col-md-4">
-              <img src={group_placeholder}/>
+class Group extends React.Component {
+  constructor(props) {
+    super(props);
+    var self = ''
+    var key = this.props.match.params.key;
+    this.state = { values: [] };
+    this.joined = false;
+  }
+
+  buttonClicked(event) {
+    var self = this;
+    if (self.state.joined != true) {
+      alert("Joined Group");
+      self.state.joined = true;
+      auth.onAuthStateChanged(function(user){
+        if (user) {
+          var userkey = '';
+          var ref = fire.database().ref("users");
+          ref.orderByChild("email").equalTo(user.email).on("value", function(snapshot){
+          snapshot.forEach(function(data) {
+            console.log(data.key);
+            userkey = data.key;
+          })
+        })
+        fire.database().ref("users/" + userkey + "/groups").update([window.location.pathname.split('/group/')[1]]);
+        fire.database().ref("groups/" + window.location.pathname.split('/group/')[1] + "/member_list").update([userkey])
+        console.log("1");
+        }
+      });
+    }
+    else {
+      alert("Already in Group");  
+      console.log("0");
+    }
+    event.preventDefault();
+  }
+
+  componentDidMount() {
+    var self = this;
+    auth.onAuthStateChanged(function (user) {
+      this.state = {
+        currentUser: user,
+      }
+      var ref = fire.database().ref("users");
+      ref.orderByChild("email").equalTo(user.email).on("value", function(snapshot){
+        snapshot.forEach(function(data) {
+          console.log(data);
+          console.log(data.ref_.path.pieces_[1]);
+          var groups = data.child("groups");
+          var x = document.getElementById("join_group");
+          groups.forEach(function(data2){
+            if (data2.val() === window.location.pathname.split('/group/')[1]) {
+              console.log(data2.val());
+              self.state.joined = true;
+            }
+          })
+          if (self.state.joined != true) {
+            x.value = "  Join Group  ";
+          }
+          else {
+            x.value = "Already Joined";
+          }
+        });
+      })
+    });  
+  }
+
+  render() {
+    return (
+      <header>
+        <Navbar bg="light" expand="ex-lg">
+          <Navbar.Brand href="./main">Meetup</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse>
+            <Nav className="ml-auto">
+              <NavItem className="ml-auto">
+                <Nav.Link href="./profile">Profile</Nav.Link>
+              </NavItem>
+              <NavItem className="ml-auto">
+                <Nav.Link className="ml-auto" href="/create_group">Create Group</Nav.Link>
+              </NavItem>
+              <NavItem className="ml-auto">
+                <Nav.Link className="ml-auto" href="#">Logout</Nav.Link>
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <main>
+          <div class="container">
+            <div class="row group_row">
+              <div class="col-md-4">
+                <img src={group_placeholder}/>
+                <input id="join_group" class="btn btn-info" type="button" value= "  Join Group  " onClick={this.buttonClicked.bind(this)}></input>
+              </div>
+              <div class="col-xs-4">
+                <h3>Group Name goes here plus some more text</h3>
+                <p>Bio goes here plus some more text</p>
+              </div>
             </div>
+            <div>
+          </div>
+          </div> 
+          <div class="container">
+            <div class="row group_row">
+              <div class="col-xs-4">
+                <h3 class="py-4">Events</h3>
+                <div class="card group_card">
+                  <div class="card-body">
+                    <h5 class="card-title">Event Title</h5>
+                    <div class="card-text">
+                      <p>Event Details </p>
+                    </div>
+                    <div class="card-bottom">
+                      <img src="#" alt="profile_img"/>
+                      <form>
+                        <div class="text-right">
+                          <button id="join_event" class="btn btn-info" type="button" name="join_event">Join Event</button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-4 offset-md-1">
+                <h3 class="py-4">Members</h3>
+                <div class="card member_card">
+                  <div class="card-body">
+                    <h5 class="card-title">Organizer</h5>
+                    <div class="card-img">
+                      <img src="#" alt="member_image"/>
+                    </div>
+                  </div>
+                </div>
+                <div class="card member_card">
+                  <div class="card-body">
+                    <h5 class="card-title">Members</h5>
+                    <div class="card-img">
+                      <img src="#" alt="member_image"/>
+                      <img src="#" alt="member_image"/>
+                      <img src="#" alt="member_image"/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> 
+          <div class="container">
+            <div class="row group_row">
+              <h3>Discussion</h3>
+            </div>
+            <div class="row">
             <div class="col-xs-4">
-              <h3>Group Name goes here plus some more text</h3>
-              <p>Bio goes here plus some more text</p>
+              <form action="#" method="get">
+                <div class="form-group">
+                  <textarea id="groupdis_text" class="form-control dis_card" type="text" rows="4" placeholder="Enter your message here" name="groupdis_text" required></textarea>
+                  <div class="text-right">
+                    <button id="submit_message" class="btn btn-primary" type="button">Enter message</button>
+                  </div>
+                </div>
+              </form>
             </div>
-          </div>
-        </div> 
-        <div class="container">
-          <div class="row group_row">
-            <div class="col-xs-4">
-              <h3 class="py-4">Events</h3>
-              <div class="card group_card">
-                <div class="card-body">
-                  <h5 class="card-title">Event Title</h5>
-                  <div class="card-text">
-                    <p>Event Details </p>
-                  </div>
-                  <div class="card-bottom">
-                    <img src="#" alt="profile_img"/>
-                    <form>
-                      <div class="text-right">
-                        <button id="join_event" class="btn btn-info" type="button" name="join_event">Join Event</button>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
             </div>
-            <div class="col-xs-4 offset-md-1">
-              <h3 class="py-4">Members</h3>
-              <div class="card member_card">
-                <div class="card-body">
-                  <h5 class="card-title">Organizer</h5>
-                  <div class="card-img">
-                    <img src="#" alt="member_image"/>
-                  </div>
-                </div>
-              </div>
-              <div class="card member_card">
-                <div class="card-body">
-                  <h5 class="card-title">Members</h5>
-                  <div class="card-img">
-                    <img src="#" alt="member_image"/>
-                    <img src="#" alt="member_image"/>
-                    <img src="#" alt="member_image"/>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> 
-        <div class="container">
-          <div class="row group_row">
-            <h3>Discussion</h3>
-          </div>
-          <div class="row">
-          <div class="col-xs-4">
-            <form action="#" method="get">
-              <div class="form-group">
-                <textarea id="groupdis_text" class="form-control dis_card" type="text" rows="4" placeholder="Enter your message here" name="groupdis_text" required></textarea>
-                <div class="text-right">
-                  <button id="submit_message" class="btn btn-primary" type="button">Enter message</button>
-                </div>
-              </div>
-            </form>
-          </div>
-          </div>
-        </div> 
-      </main>
-    </header>
-  );
+          </div> 
+          
+        </main>
+      </header>
+    );
+  }
 }
 
 export default Group;
